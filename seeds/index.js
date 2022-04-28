e08578a5ba176d92ca5ac55f3d2e90180c4efe6e
kyle <elykkyle@gmail.com>
2022-04-27 22:04:52 -0500

    bootstrapify almost everything.

--- seeds/index.js
+++ seeds/index.js
@@ -4,33 +4,38 @@ const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
 
 main()
-    .then(() => {
-        console.log('MONGO CONNECTION OPEN!');
-    })
-    .catch((err) => {
-        console.log('ERROR CONNECTING TO MONGO');
-        console.log(err);
-    });
+  .then(() => {
+    console.log('MONGO CONNECTION OPEN!');
+  })
+  .catch((err) => {
+    console.log('ERROR CONNECTING TO MONGO');
+    console.log(err);
+  });
 
 async function main() {
-    await mongoose.connect('mongodb://localhost:27017/yelp-camp');
+  await mongoose.connect('mongodb://localhost:27017/yelp-camp');
 }
 
 const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
-    await Campground.deleteMany({});
-    for (let i = 0; i < 50; i++) {
-        const random1000 = Math.floor(Math.random() * 1000);
-        const camp = new Campground({
-            location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`,
-        });
-        await camp.save();
-    }
+  await Campground.deleteMany({});
+  for (let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
+    const camp = new Campground({
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+      image: 'https://source.unsplash.com/collection/483251',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut minus incidunt veniam quae earum reprehenderit animi ratione debitis facilis minima perferendis rem, vero totam distinctio dolores repudiandae cumque pariatur.',
+      price: price,
+    });
+    await camp.save();
+  }
 };
 
 seedDB().then(() => {
-    console.log('DONE!!!');
-    mongoose.connection.close();
+  console.log('DONE!!!');
+  mongoose.connection.close();
 });
