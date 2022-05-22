8deab060f4cc5fbfd4d876efb5851e1dbe66df70
kyle <elykkyle@gmail.com>
2022-05-22 15:02:11 -0500

    deleting reviews route

--- app.js
+++ app.js
@@ -126,7 +126,10 @@ app.post(
 app.delete(
   '/campgrounds/:id/reviews/:reviewId',
   catchAsync(async (req, res) => {
-    res.send('delete me!!');
+    const { id, reviewId } = req.params;
+    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/campgrounds/${id}`);
   })
 );
 
