690cbd3bfde55ed3f6e5a6330fe2c766c0494470
kyle <elykkyle@gmail.com>
2022-06-12 12:01:57 -0500

    add redirect back to original URL

--- app.js
+++ app.js
@@ -29,6 +29,9 @@ const sessionConfig = {
   },
 };
 
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(session(sessionConfig));
 app.use(flash());
 
@@ -39,11 +42,8 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride('_method'));
-app.use(express.static(path.join(__dirname, 'public')));
-
 app.use((req, res, next) => {
+  console.log(req.session);
   res.locals.currentUser = req.user;
   res.locals.success = req.flash('success');
   res.locals.error = req.flash('error');
