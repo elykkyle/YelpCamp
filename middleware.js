8bab6f4eb12487f51b27b2d94b164eef2dd2ef09
kyle <elykkyle@gmail.com>
2022-06-14 20:41:00 -0500

    add author to review, authorize review delete

--- middleware.js
+++ middleware.js
@@ -1,4 +1,5 @@
 const Campground = require('./models/campground');
+const Review = require('./models/review');
 const { campgroundSchema, reviewSchema } = require('./schemas');
 const ExpressError = require('./utils/ExpressError');
 
@@ -24,6 +25,16 @@ module.exports.isAuthor = async (req, res, next) => {
   const { id } = req.params;
   const campground = await Campground.findById(id);
   if (!campground.author.equals(req.user._id)) {
+    req.flash('error', 'You are not authorized to do that!');
+    return res.redirect(`/campgrounds/${id}`);
+  }
+  next();
+};
+
+module.exports.isReviewAuthor = async (req, res, next) => {
+  const { id, reviewId } = req.params;
+  const review = await Review.findById(reviewId);
+  if (!review.author.equals(req.user._id)) {
     req.flash('error', 'You are not authorized to do that!');
     return res.redirect(`/campgrounds/${id}`);
   }
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -31,9 +31,9 @@ router.get(
   '/:id',
   catchAsync(async (req, res) => {
     const id = req.params.id;
-    const campground = await Campground.findById(id).populate('reviews').populate('author');
-    console.log(campground);
-    console.log(req.user);
+    const campground = await Campground.findById(id)
+      .populate({ path: 'reviews', populate: { path: 'author' } })
+      .populate('author');
     if (!campground) {
       req.flash('error', 'Cannot find that campground');
       return res.redirect('/campgrounds');
