c44f48b237d804ffcb9ab8dead2b7290a6d61e78
kyle <elykkyle@gmail.com>
2022-06-21 17:37:04 -0500

    added favicon

--- app.js
+++ app.js
@@ -24,8 +24,8 @@ const sessionConfig = {
   saveUninitialized: true,
   cookie: {
     httpOnly: true,
-    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
     maxAge: 1000 * 60 * 60 * 24 * 7,
+    sameSite: true,
   },
 };
 
--- routes/users.js
+++ routes/users.js
@@ -4,13 +4,12 @@ const catchAsync = require('../utils/catchAsync');
 const passport = require('passport');
 const users = require('../controllers/users');
 
-router.get('/register', users.renderRegisterForm);
+router.route('/register').get(users.renderRegisterForm).post(catchAsync(users.register));
 
-router.post('/register', catchAsync(users.register));
-
-router.get('/login', users.renderLoginForm);
-
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login', keepSessionInfo: true }), users.login);
+router
+  .route('/login')
+  .get(users.renderLoginForm)
+  .post(passport.authenticate('local', { failureFlash: true, failureRedirect: '/login', keepSessionInfo: true }), users.login);
 
 router.get('/logout', users.logout);
 
