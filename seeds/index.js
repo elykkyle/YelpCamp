beb09b38c88d8313aa0622ce6ad1fcf75ccdb4e5
kyle <elykkyle@gmail.com>
2022-06-28 13:45:48 -0500

    set default image for campgrounds index if none. update seeds with geo

--- seeds/index.js
+++ seeds/index.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Campground = require('../models/campground');
+const Review = require('../models/review');
 const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
 
@@ -20,6 +21,7 @@ const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
   await Campground.deleteMany({});
+  await Review.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
@@ -43,6 +45,10 @@ const seedDB = async () => {
       description:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut minus incidunt veniam quae earum reprehenderit animi ratione debitis facilis minima perferendis rem, vero totam distinctio dolores repudiandae cumque pariatur.',
       price: price,
+      geometry: {
+        type: 'Point',
+        coordinates: [-93.265469, 44.9773],
+      },
       author: '629ff7dfff6c0371b37e49dd',
     });
     await camp.save();
