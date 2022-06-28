ad8610b41116eb20d08dd697996c1845cf26e3f2
kyle <elykkyle@gmail.com>
2022-06-28 10:56:13 -0500

    refactor to include entire campground object

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -2,8 +2,8 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: 'map', // container ID
   style: 'mapbox://styles/mapbox/streets-v11', // style URL
-  center: campgroundCoordinates, // starting position [lng, lat]
+  center: campground.geometry.coordinates, // starting position [lng, lat]
   zoom: 9, // starting zoom
 });
 // Create a default Marker and add it to the map.
-const marker1 = new mapboxgl.Marker().setLngLat(campgroundCoordinates).addTo(map);
+const marker1 = new mapboxgl.Marker().setLngLat(campground.geometry.coordinates).addTo(map);
